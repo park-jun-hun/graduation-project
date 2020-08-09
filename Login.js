eafb25c34563082bda1376bb162f84504e37700e
park-junghun <wjdgns001100@gmail.com>
2020-08-09 17:34:20 +0900

    Screen

--- Login.js
+++ Login.js
@@ -14,7 +14,7 @@ export default function LoginScreen({navigation}) {
        
      <Container>
          <ImageBackground style={{width: '100%', height: '75%'}} 
-         source={require('./pic/main.png')}/>
+         source={require('./pic/main2.png')}/>
      
          <Textstyle>
             <LoginText/>
