d071c1d53517a39725f80bb54ed2dede64f29cc3
Lee-so-young <63101648+Lee-so-young@users.noreply.github.com>
2020-11-28 16:51:20 +0900

    Lee.js & score.js

--- park/score.js
+++ park/score.js
@@ -2,6 +2,8 @@ import {Text, View, Easing, Button} from "react-native";
 import React from "react";
 import styled from "styled-components/native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import * as firebase from 'firebase';
+import "firebase/database";
 
 import {
   Dimensions
@@ -12,10 +14,41 @@ const winHeight = Dimensions.get('window').height;
 
 
 export default function ScoreScreen({navigation}) {
-    return (
+  
+    const firebaseConfig={
+        apiKey: "AIzaSyDLMuHBhU91JCg3-JT48cX5RQM1Oasov_Q",
+        authDomain: "graduation-4651e.firebaseapp.com",
+        databaseURL: "https://graduation-4651e.firebaseio.com",
+        projectId: "graduation-4651e",
+        storageBucket: "graduation-4651e.appspot.com",
+        messagingSenderId: "449238174270",
+        appId: "1:449238174270:web:3f2168bb35db1e8c025f6f",
+        measurementId: "G-17QJW69D5K"
+      };
+
+      
+      //firebase.initializeApp(firebaseConfig);
+
+      //덮어쓰기
+   /* const usersRef=firebase.database().ref('Score/').set({
+    Score1:20,
+    Score2:30,
+    Score3:40,
+    });
+    */
+
+    //console.log(usersRef.key);
+    let message;
+    //읽어오기
+    const m = firebase.database().ref('Score').on('value',function(snapshot){
+     message =snapshot.val();
+     console.log(message);
+    });
+
+      return (
         <Container>
             <Content>
-            <Text>환영합니다.</Text>
+      <Text>{message}</Text>
             </Content>
             <Start>
                 <Text onPress={() => navigation.navigate('HomeScreen')}>
