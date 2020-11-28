f105301938e63ce7703a2dfae545808d66fed0b8
Lee-so-young <63101648+Lee-so-young@users.noreply.github.com>
2020-11-28 12:19:02 +0900

    Update Park.js

--- Park.js
+++ Park.js
@@ -1,151 +1,46 @@
-import {Text, View, Easing, Button} from "react-native";
 import React from "react";
+import {View, Text, Button} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
 import styled from "styled-components/native";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import P1 from "./svg/park1";
-import P2 from "./svg/park2";
-import P3 from "./svg/park3";
-import P4 from "./svg/park4";
-import {connect} from 'react-redux';
-import {mapDispatchToProps, mapStateToProps} from "./Mapping";
 
-let user_name="";
-function ParkScreen(props,{navigation}) {
-    user_name=props.name;
+let user_name='박정훈';
+export default function ParkScreen({navigation}) {
     return (
-        <Container>
-        <Main>
-          <IconStyle>
-              <Text>사진</Text>
-          </IconStyle>
-          <MainText>
-              {user_name}님 환영합니다.
-       </MainText>
-        <MainText2>
-              {user_name}님 정보를 확인하세요.
-       </MainText2>
-       
-        </Main>
-
-             <Bottom>
-                 <Btn1>
-                     <P1 style={{position:'absolute'}}
-                     onPress={() => props.navigation.navigate('RecordScreen')}/>
-             <Text_style>기록다시듣기 </Text_style>
-                 </Btn1>
-                 <Btn2>
-                 <P2 style={{position:'absolute', top:13}}
-                     onPress={() => props.navigation.navigate('ScoreScreen')}/>
-                 <Text_style>점수확인하기 </Text_style>
-        </Btn2>
-        <Btn3>
-        <P3 style={{position:'absolute', top:13}}
-                     onPress={() => props.navigation.navigate('CheckScreen')}/>
-
-        <Text_style>유사도 확인 </Text_style>
-        </Btn3>
-        <Btn4>
-        <P4 style={{position:'absolute', top:13}}
-                     onPress={() => props.navigation.navigate('InfoScreen')}/>
-
-        <Text_style>내정보 확인 </Text_style>
-        </Btn4>
-             </Bottom>
-        </Container>
-    );
+       <Container>
+           <TEXT>
+               {user_name}님 환영합니다!
+           </TEXT>
+           <Text></Text>
+           <TEXT>
+               원하시는 항목을 선택해주세요.
+           </TEXT>
+           <Btn1>
+       <P1/>
+       </Btn1>
+       <BTEXT>
+           DDD
+       </BTEXT>
+       </Container>
+    )
 }
-
 const Container = styled.View`
   flex:1;
-  background-color:#77AF9C;
+  background : white;
 `;
-const Main =styled.View`
-  flex:0.4;
+const TEXT = styled.Text`
+    top:10%;
+    left : 35%;
+    font_size:18px;
 `;
-
-const Bottom = styled.View`
-    flex: 0.6;
-`;
-const MainText = styled.Text`
-  font-size:25px;
-  font-style: italic;
-  top:45%;
-  left:35%;
-`;
-const MainText2 = styled.Text`
-  font-size:18px;
-  font-style: italic;
-  top:45%;
-  left:35%;
-`;
-const IconStyle =styled.View`
-  position:absolute;
-  top:30%;
-  left:5%;
-`;
-const Btn1= styled.View`
+const BTEXT = styled.Text`
     position:absolute;
-    align-items:center;
-    background : white;
-    top:3%;
-    left:3%;
-    padding-right:45%;
-    padding-top:45%;
-    border-bottom-left-radius: 40px;
-    border-bottom-right-radius: 40px;
-    border-top-left-radius: 40px;
-    border-top-right-radius: 40px;
+    top:60%;
+    left : 10%;
+    font_size:18px;
 `;
-const Btn2= styled.View`
+const Btn1 = styled.View`
     position:absolute;
-    flex:0.2;
-    align-items:center;
-    background : white;
-    top:3%;
-    right:3%;
-    padding-right:45%;
-    padding-top:45%;
-    border-bottom-left-radius: 40px;
-    border-bottom-right-radius: 40px;
-    border-top-left-radius: 40px;
-    border-top-right-radius: 40px;
+    top:40%;
+    left : 10%;
 `;
-const Btn3= styled.View`
-    position:absolute;
-    flex:0.2;
-    align-items:center;
-    background : white;
-    bottom:3%;
-    left:3%;
-    padding-right:45%;
-    padding-top:45%;
-    border-bottom-left-radius: 40px;
-    border-bottom-right-radius: 40px;
-    border-top-left-radius: 40px;
-    border-top-right-radius: 40px;
-`;
-const Btn4= styled.View`
-    position:absolute;
-    flex:0.2;
-    align-items:center;
-    background : white;
-    bottom:3%;
-    right:3%;
-    padding-right:45%;
-    padding-top:45%;
-    border-bottom-left-radius: 40px;
-    border-bottom-right-radius: 40px;
-    border-top-left-radius: 40px;
-    border-top-right-radius: 40px;
-`;
-const Text_style=styled.Text`
-    position:absolute;
-    font-size:18px;
-    top: 145%
-    color:#222222;
-    font-weight:bold;
-    font-style: italic;
-    
-`;
-
-export default connect(mapStateToProps, mapDispatchToProps)(ParkScreen);
