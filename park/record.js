675afca510c0e059ecdc8dd4caa0bf18ebeda2ce
park-jun-hun <63997284+park-jun-hun@users.noreply.github.com>
2021-01-05 00:53:27 +0900

    RNFS
    파일 접근 헤더 파일 추가

--- park/record.js
+++ park/record.js
@@ -2,20 +2,31 @@ import {Text, View, Easing, Button} from "react-native";
 import React from "react";
 import styled from "styled-components/native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-
 import {
   Dimensions
 } from 'react-native';
 const winWidth = Dimensions.get('window').width;
 const winHeight = Dimensions.get('window').height;
 
+// import {DocumentPicker} from "expo";
+// DocumentPicker.getDocumentAsunc({
+//     type:"audio/*",
+//     copyToCacheDirectory: true  // Expo.FileSystme.CacheDirectory
+// })
 
+import * as RNFS from 'react-native-fs'
+RNFS.readFile(filePath,'ascii').then(res => {
+
+})
+.catch(err => {
+    console.log(err.message, err.code);
+});
 
 export default function RecordScreen({navigation}) {
     return (
         <Container>
             <Content>
-            <Text>환영합니다.</Text>
+            <Text>음악 가져오기 </Text>
             </Content>
             <Start>
                 <Text onPress={() => navigation.navigate('HomeScreen')}>
