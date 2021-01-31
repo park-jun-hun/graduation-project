0c79293ee96577e3c5334bfc93fb1a7dda3faa8e
park-jun-hun <63997284+park-jun-hun@users.noreply.github.com>
2021-01-31 16:25:08 +0900

    2021.01.31
    upload, learning result (not view)

--- park/record.js
+++ park/record.js
@@ -1,51 +1,15 @@
-import {Text, View, Easing, Button} from "react-native";
-import React from "react";
-import styled from "styled-components/native";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import {
-  Dimensions
-} from 'react-native';
-const winWidth = Dimensions.get('window').width;
-const winHeight = Dimensions.get('window').height;
-
-// import {DocumentPicker} from "expo";
-// DocumentPicker.getDocumentAsunc({
-//     type:"audio/*",
-//     copyToCacheDirectory: true  // Expo.FileSystme.CacheDirectory
-// })
-
-import * as RNFS from 'react-native-fs'
-RNFS.readFile(filePath,'ascii').then(res => {
-
-})
-.catch(err => {
-    console.log(err.message, err.code);
-});
+import React, { Component} from 'react';
+import { WebView ,Text} from 'react-native-webview';
 
 export default function RecordScreen({navigation}) {
     return (
-        <Container>
-            <Content>
-            <Text>음악 가져오기 </Text>
-            </Content>
-            <Start>
-                <Text onPress={() => navigation.navigate('HomeScreen')}>
-                    시작하기
-                </Text>
-            </Start>
-
-        </Container>
-    );
+        
+           
+        <WebView
+        automaticallyAdjustContentInsets={false}
+          source={{uri: 'https://graduation-project-fzvhk.run.goorm.io/graduation_project/index.html'}}
+            style={{marginTop:30, marginLeft:0, width:"250%",marginLeft:-5}}
+        />
+       
+      );
 }
-
-const Container = styled.View`
-  flex:1;
-`;
-const Content = styled.View`
-    align-items:center;
-    top:50%
-`;
-const Start = styled.View`
-    top:90%;
-    left:80%;
-`;
